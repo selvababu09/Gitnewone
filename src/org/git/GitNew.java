90482ebb8028e624b0c6b025c05f6c69f59b55c1
selvababu09 <selvababu09@gmail.com>
2023-06-02 10:03:17 +0530

    Anu added

--- src/org/git/GitNew.java
+++ src/org/git/GitNew.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class GitNew {
+	
+	private void Anu() {
+System.out.println("Anu added the code");
+	}
 
 	public static void main(String[] args) {
 System.out.println("Data");
