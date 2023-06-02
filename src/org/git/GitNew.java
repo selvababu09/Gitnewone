6c2110d20fe933c22b946b743e8e6274fbe758c4
selvababu09 <selvababu09@gmail.com>
2023-06-02 10:07:38 +0530

    Bala added

--- src/org/git/GitNew.java
+++ src/org/git/GitNew.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class GitNew {
+	
+	private void Bala() {
+System.out.println("Bala added");
+	}
 
 	public static void main(String[] args) {
 System.out.println("Data");
