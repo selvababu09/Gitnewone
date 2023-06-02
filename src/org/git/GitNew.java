9b658450d8ec32bffef14d07e09957edfccabc5a
selvababu09 <selvababu09@gmail.com>
2023-06-02 10:15:56 +0530

    priya added

--- src/org/git/GitNew.java
+++ src/org/git/GitNew.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class GitNew {
+	
+	private void priya() {
+System.out.println("priya added");
+	}
 
 	public static void main(String[] args) {
 System.out.println("Data");
